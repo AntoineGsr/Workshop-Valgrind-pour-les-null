e464fdec1c060fde74937edb8c22eca44f4bee16
Gasser <antoine.gasser@epitech.eu>
2022-03-11 06:14:31 -0500

    [ADD] ex02

---
+++ ex02/main.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** main
+*/
+
+#include "../include/workshop_display.h"
+
+static const char *encrypted_string(const char *str, int size)
+{
+    char *cesar = malloc(sizeof(char) * size);
+    
+    cesar = memfrob(str, size);
+    return (cesar);
+}
+
+int main(const int ac, const char **av)
+{
+    char *tmp = NULL;
+    char *str = "sE_\nLCDN\n^BO\nLFKM";
+
+    tmp = encrypted_string(str, strlen(str));
+    printf("%s\n", tmp);
+    free(tmp);
+    free(str);
+    return (0);
+}
